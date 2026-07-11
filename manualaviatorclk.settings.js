025e56c87a5f2143259c941a24b08c6f487a32b7
Kas-Akari <37886456+Kas-Akari@users.noreply.github.com>
2026-07-12 00:31:55 +0200

    removed texinput dependencies

--- manualaviatorclk.settings.js
+++ manualaviatorclk.settings.js
@@ -30,25 +30,6 @@
         writeSettings();
       }
     },
-    'ICAO Airport': function() {
-      require("textinput").input({
-        text: settings.icao || '',
-        prompt: "ICAO (4 chars)"
-      }).then(v => {
-        if (v === undefined) return;
-
-        v = v.toUpperCase().trim().replace(/[^A-Z0-9]/g, '').slice(0, 4);
-        if (v.length !== 4) {
-          E.showMessage("ICAO invalid");
-          setTimeout(() => E.showMenu(menu), 1000);
-          return;
-        }
-
-        settings.icao = v;
-        writeSettings();
-        E.showMenu(menu);
-      });
-    },
   };
 
   E.showMenu(menu);
